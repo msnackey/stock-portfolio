3ea68eb6d21be47ba27f3f6dd7c42edf0ea8bbc9
msnackey <m.snackey@gmail.com>
2022-12-25 13:03:39 +0100

    Edited the settings configuration

--- manage.py
+++ manage.py
@@ -2,11 +2,12 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from decouple import config
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stock_portfolio.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', config('settings'))
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
